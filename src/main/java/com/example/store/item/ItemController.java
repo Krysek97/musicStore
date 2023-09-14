33367063a2236d05a6512c1eefd9a570ac29e6d3
Natalia <natalia.krysaa@gmail.com>
2023-09-14 14:51:01 +0200

    item controller

--- src/main/java/com/example/store/item/ItemController.java
+++ src/main/java/com/example/store/item/ItemController.java
@@ -1,7 +1,42 @@
 package com.example.store.item;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
+@RequestMapping("/item")
 public class ItemController {
+
+    private final ItemService itemService;
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @PostMapping("/add")
+    public Item add(Item item){
+        return itemService.add(item);
+    }
+
+    @PostMapping("/delete/{id}")
+    public void deleteById(@PathVariable Long id){
+        itemService.deleteById(id);
+    }
+
+    @GetMapping("/{category}")
+    public List<Item> allByCategory(@PathVariable String category){
+        return itemService.allByCategory(category);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Item> findById(Long id){
+        return itemService.findById(id);
+    }
+
 }
